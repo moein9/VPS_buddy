fdfb642c1f5c05780b3f176a6c0bb4ae768d4b43
moSec <50665881+moein9@users.noreply.github.com>
2021-11-20 23:26:04 +0300

    Update VPS_buddy.sh

--- VPS_buddy.sh
+++ VPS_buddy.sh
@@ -52,9 +52,15 @@ sudo apt-get install -y xargs
 sudo apt-get install -y httpie
 sudo apt-get install -y curl
 sudo apt-get install -y libcurl4-openssl-dev
-sudo apt-get install -y docker
 sudo apt-get install -y dnsutils
 sudo apt-get install -y python3-pip
+
+echo "Installing docker"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce
+echo "done"
 
 
 sudo apt install figlet
