26c384c01a392c4035666d07af24a65b0bfcb7cd
moSec <50665881+moein9@users.noreply.github.com>
2023-02-24 21:01:51 +0300

    go version update

--- VPS_buddy.sh
+++ VPS_buddy.sh
@@ -109,16 +109,20 @@ cd $ToolsPath/
  then
  
 echo "Installing Golang"
-          wget https://dl.google.com/go/go1.17.6.linux-amd64.tar.gz
-          sudo tar -xvf go1.17.6.linux-amd64.tar.gz
-          sudo mv go /usr/local
-          export GOROOT=/usr/local/go
-          export GOPATH=$HOME/go
-          export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
-          echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
-          echo 'export GOPATH=$HOME/go' >> ~/.bash_profile      
-          echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile 
-          source ~/.bash_profile		
+          wget https://go.dev/dl/go1.20.1.linux-arm64.tar.gz
+	  sudo rm -rf /usr/local/go
+          sudo tar -C /usr/local -xzf go*.tar.gz
+#           sudo mv go /usr/local
+#           export GOROOT=/usr/local/go
+#           export GOPATH=$HOME/go
+#           export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+#           echo 'export GOROOT=/usr/local/go' >> ~/.bash_profile
+#           echo 'export GOPATH=$HOME/go' >> ~/.bash_profile      
+#           echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.bash_profile 
+#           source ~/.bash_profile
+	  echo 'export PATH=$PATH:/usr/local/go/bin:/root/go/bin' >>~/.profile
+	  sudo sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile'
+	  export PATH=$PATH:/usr/local/go/bin
 	
 cd ~/
  fi
